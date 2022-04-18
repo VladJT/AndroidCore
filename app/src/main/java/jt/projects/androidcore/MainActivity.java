c15e9928561fe504947875bfd275b710131e385d
Богданков Владислав <onlyrus@hotmail.com>
2022-04-18 19:04:43 +0300

    GB_1

--- app/src/main/java/jt/projects/androidcore/MainActivity.java
+++ app/src/main/java/jt/projects/androidcore/MainActivity.java
@@ -1,12 +1,17 @@
 package jt.projects.androidcore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Основные файлы в проекте:
@@ -24,33 +29,83 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Switch mySwitch;
+    Button button;
+    EditText editText1;
+    EditText editText2;
+    TextView textView;
+    CheckBox checkBox;
+    CalendarView calendarView;
+
     private Integer arg1;
     private Integer arg2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        final EditText editText1 = findViewById(R.id.editText1);
-        final EditText editText2 = findViewById(R.id.editText2);
-        final TextView textView = findViewById(R.id.textView);
+        initComponents();
+
+        setListeners();
+    }
+
+
+    private void initComponents() {
+        mySwitch = findViewById(R.id.switch1);
+        button = findViewById(R.id.button);
+        editText1 = findViewById(R.id.editText1);
+        editText2 = findViewById(R.id.editText2);
+        textView = findViewById(R.id.textView);
+        checkBox = findViewById(R.id.checkBox);
+        calendarView = findViewById(R.id.calendarView);
+    }
+
+
+    private void setListeners() {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
                     arg1 = Integer.valueOf(editText1.getText().toString());
                     arg2 = Integer.valueOf(editText2.getText().toString());
-                    if (arg1.equals(arg2)) {
+                    boolean result;
+                    if (mySwitch.isChecked()) result = arg1.equals(arg2);
+                    else result = (arg1==arg2);
+                    if (result) {
                         textView.setText("Равно!");
                     } else {
                         textView.setText("НЕ Равно!");
                     }
-                }
-                catch (Exception e){
+                } catch (Exception e) {
                     textView.setText("Введите число");
                 }
             }
         });
+
+        mySwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mySwitch.isChecked())
+                    Toast.makeText(v.getContext(), "Checked EQUALS", Toast.LENGTH_SHORT).show();
+                else Toast.makeText(v.getContext(), "Unchecked EQUALS (use =)", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), checkBox.isChecked()+"", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(calendarView.getContext(), dayOfMonth+"."+month+"."+year, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
 
 }
